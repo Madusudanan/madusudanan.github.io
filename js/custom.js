7be927a5a93afa664f6e058f7e3e8ae69de3285b
madusudanan <b.n.madusudanan@gmail.com>
2015-05-24 23:38:21 +0530

    CSS/JS optimization and minification

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,1 @@
-//Parse GET parameters and do appropriate mouse actions
-function categoryNav(){
-    $( document ).ready(function() {
-    var query = window.location.search.substring(1);
-    var vars = query.split('&');
-    for (var i = 0; i < vars.length; i++) {
-        $('#' +vars[i]).click();
-    }
-    });
-}
+function categoryNav(){$(document).ready(function(){for(var n=window.location.search.substring(1),c=n.split("&"),o=0;o<c.length;o++)$("#"+c[o]).click()})}
